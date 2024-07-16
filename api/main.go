ff80ac9fbd935915032220a00598a93a97f932eb
Adrien Parrochia <adrien.parrochia@adevinta.com>
2024-07-16 18:05:00 +0200

    [api] clean main.go

--- api/main.go
+++ api/main.go
@@ -10,56 +10,23 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
-	"os/exec"
 	"strings"
 	"time"
-
-	tf "github.com/wamuir/graft/tensorflow"
 )
 
-type Prompt struct {
-	model         *tf.SavedModel
-	tokenizerName string
-}
+type Prompt struct{}
 
 type Record struct {
 	Text   string `json:"name"`
 	Result string `json:"comment"`
 }
 
-type Args struct {
-	ModelDir string
-}
-
 func main() {
-	args := Args{ModelDir: "/app/models"}
-
-	p := Prompt{
-		tokenizerName: "Salesforce/codet5-base",
-	}
-
-	if err := p.loadModel(args.ModelDir); err != nil {
-		log.Fatalf("fail to load model: %v", err)
-	}
-
-	defer func() {
-		if err := p.close(); err != nil {
-			log.Printf("fail to close model: %v", err)
-		}
-	}()
+	p := Prompt{}
 
 	if err := p.predictFromDataset(); err != nil {
 		log.Fatalf("fail to predict from dataset: %v", err)
 	}
-}
-
-func (p *Prompt) loadModel(modelDir string) error {
-	var err error
-	p.model, err = tf.LoadSavedModel(modelDir, []string{"serve"}, nil)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func (p *Prompt) predictFromDataset() error {
@@ -164,74 +131,4 @@ func (p *Prompt) runPredict(text string) (string, error) {
 	}
 
 	return tokenizeResponse.Comment, nil
-}
-
-// Fonction pour décoder les tokens générés
-func (p *Prompt) decodeTokens(tokenizerPath string, generatedTokens []float32) (string, error) {
-	// Implémenter le décodage selon votre tokenizer spécifique.
-	// Cette partie dépend de votre tokenizer utilisé pour l'encodage.
-
-	// Exemple simplifié :
-	decodedText := "Decoded text from tokens"
-	return decodedText, nil
-}
-
-func (p *Prompt) waitTokenizerContainer() error {
-	for {
-		cmd := exec.Command("docker", "inspect", "-f", "{{.State.Running}}", "tokenizer_container")
-		var out bytes.Buffer
-		cmd.Stdout = &out
-		if err := cmd.Run(); err != nil {
-			return err
-		}
-		if out.String() == "true\n" {
-			break
-		}
-		time.Sleep(1 * time.Second)
-	}
-	return nil
-}
-
-type tokenizeResp struct {
-	InputIDs      [][]int32 `json:"input_ids"`
-	AttentionMask [][]int32 `json:"attention_mask"`
-}
-
-func (p *Prompt) tokenizeWithPython(text string) (tokenizeResp, error) {
-	var tokenize tokenizeResp
-	/*if err := p.waitTokenizerContainer(); err != nil {
-		return tokenize, err
-	}*/
-
-	cmd := exec.Command("docker", "exec", "tokenizer_container", "python3", "tokenizer_script.py",
-		text, p.tokenizerName)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	if err := cmd.Run(); err != nil {
-		return tokenize, err
-	}
-
-	if err := json.Unmarshal(out.Bytes(), &tokenize); err != nil {
-		return tokenize, err
-	}
-
-	log.Printf("tokenize found: %+v", tokenize)
-
-	return tokenize, nil
-}
-
-func convertFloat32ArrayToString(arr [][][]float32) string {
-	var sb strings.Builder
-	for _, sentence := range arr {
-		for _, word := range sentence {
-			for _, char := range word {
-				sb.WriteString(fmt.Sprintf("%c", int(char)))
-			}
-		}
-	}
-	return sb.String()
-}
-
-func (p *Prompt) close() error {
-	return p.model.Session.Close()
 }
